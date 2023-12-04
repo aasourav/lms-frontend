607b89177a06ab37e8d442d3b3c1b3f71ee418ce
Ahsan Amin <ahsan.amin@headless.ltd>
2023-12-04 09:24:43 +0600

    project setup update 1

---
+++ src/components/molecules/Components.tsx
@@ -0,0 +1,24 @@
+import { down } from "styled-breakpoints";
+import styled from "styled-components";
+
+export const PaddedContainer = styled.div`
+  width: 100%;
+  padding: 0 15%;
+  display: flex;
+  flex-flow: column;
+  align-items: center;
+  ${down("lg")} {
+    padding: 0 5%;
+  }
+  ${down("sm")} {
+    padding: 0 5%;
+  }
+  ${down("xs")} {
+    padding: 0 1rem;
+  }
+`;
+
+export const MaxWidthContainer = styled.div`
+  max-width: 1360px;
+  width: 100%;
+`;
